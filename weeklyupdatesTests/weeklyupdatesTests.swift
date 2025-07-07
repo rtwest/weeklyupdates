a601c969a9fc38ecbf43f983ba00ee05a214b1da
Ryan West <rtwest@gmail.com>
2025-07-07 05:50:42 -0400

    Initial Commit

---
+++ weeklyupdatesTests/weeklyupdatesTests.swift
@@ -0,0 +1,17 @@
+//
+//  weeklyupdatesTests.swift
+//  weeklyupdatesTests
+//
+//  Created by Ryan West on 7/7/25.
+//
+
+import Testing
+@testable import weeklyupdates
+
+struct weeklyupdatesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ weeklyupdatesUITests/weeklyupdatesUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  weeklyupdatesUITestsLaunchTests.swift
+//  weeklyupdatesUITests
+//
+//  Created by Ryan West on 7/7/25.
+//
+
+import XCTest
+
+final class weeklyupdatesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
